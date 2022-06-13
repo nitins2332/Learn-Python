0e7597d5d9a3d3a5f42cf9464673cedf10a10414
Nitins <nitins2332@gmail.com>
2022-06-13 10:47:15 +0530

    Learn Python

---
+++ print_func.py
@@ -0,0 +1,601 @@
+# print("hello world")
+
+# print("helel workld")
+
+# print("hello 'world' ")
+
+# print("hello \"world\" world")
+
+# print('I\'m harshit')
+
+# print("line a\nLine b\nline c")
+
+# print("name\tHarshit")
+
+# print("this is \ backslash\\")
+
+# print("hell\blo")
+
+# print("Line A \\n Line B")
+
+# print("\\\" \\\'")
+
+# print("this is \\\\ double backslash")
+# print("this is /\/\/\/\/\ mountain")
+# print("he is \tawesome")
+# print("\\\" \\n \\t \\\'")
+
+
+# shortcut for print backslash
+# print(r"line A \n line B")
+
+# print("❤❤❤❤💛💔❣")
+
+# print("\U0001f600")
+
+# print(3+4)
+
+# print("hello world")
+
+# if 5 > 2:
+#   print("Five is greater than two!")
+
+# if 5 > 2:
+#     print("Five is greater than two!")  
+
+# if 8 > 3:
+#     print("8 is greater then 3")
+#     print("this is another line")
+
+# x = 5
+# y = "hello world"
+
+# print(x, y)
+
+"""
+syntex for multiple line comment
+# this is also use of multiple line of comment
+this is a comment
+this is a another line of comment
+this is a third line of comment
+"""
+
+
+# x= 4
+# x= "Sally"
+# print(x)
+
+# x = str(3)
+# y = int(3)
+# z = float(3)
+
+# print(x,y,z)
+
+# x = 5
+# y = "nitins"
+
+# print(type(x))
+# print(type(y))
+
+
+# a = 4
+# A = "sally"
+# print(a,A)
+
+# _myVar = 34
+# print(_myVar)
+
+
+# myVariableName = "John" # camel case
+# MyVariableName = "Nitins" # pascal case
+# my_variable_name = "sutrakar" # snake case
+# print(myVariableName)
+# print(MyVariableName)
+# print(my_variable_name)
+
+# x, y,z = "white","black", "green"
+# print(x)
+# print(y)
+# print(z)
+
+
+# x = y = z = "black"
+# print(x)
+# print(y)
+# print(z)
+
+
+# fruits = ["apple","banana","cherry"]
+# x,y,z = fruits
+# print("The value of x is",x)
+# print(y)
+# print(z)
+
+# from tkinter import N
+
+
+# x = "python is awesome"
+# y = "python is wonderful"
+# z = "python is butterfly"
+# print(x + y + z)
+
+
+# x = "awesome"
+# def myfunc():
+#    global x 
+#    x = "awesome"
+    # print("python is "+ x)
+
+# myfunc()  
+# print(x)
+
+
+# x = "awesome"
+
+# def myfunc():
+#     global x
+#     x = "fantastic"
+
+# print(x)
+# myfunc()    
+
+# print("print is "+ x)
+
+
+# x = 5
+# print(type(x))
+# x = 1j
+# print(type(x))
+
+# x = ["apple", "banana", "cherry"]
+# print(x)
+# x = ("apple", "banana", "cherry")
+# print(x)
+# x = range(6)
+# print(x)
+
+# y = {"name" : "John", "age": 36}
+# print(type(y))
+
+# x = b"Hello"
+# print(type(x))
+# from tkinter import Y
+
+
+# z = -345645453434
+# print(type(z))
+
+
+# y = 12E4
+# print(y)
+
+# from re import X
+
+
+# x = 3 + 5j
+# print(type(x))
+# print(x)
+
+# x = 1
+# y = 2.8
+# z = 2j
+
+# a = float(x)
+# b = int(y)
+# c = complex(x)
+# d = int(z)
+# print(d)
+
+# print(a,b,c)
+
+# import random
+# print(random.randrange(1,56))
+
+# y = str(3)
+# print(y)
+# z = str(3.0); print(z)
+
+# a = """Lorem my name is nitins ,
+# i am completed my Bechlor from
+# university of allahabad
+# """
+# print(a)
+
+# a = "hello, world"
+# print(len(a))
+
+# for x in "banana":
+#     print(x)
+
+
+# txt = "The best things in life are free"
+# if "free" in txt:
+#     print("yes, 'free' is present.")
+# else:
+#     print("this word is not present in txt")
+
+# b = "      Hello World!"
+# print(b.strip()) # remove whitespace is the space before and/or after actual text
+# print(b)
+
+# v = "Hello World!!"
+# print(v)
+# print(v.replace("H", "J"))
+# print(v.split(" "py))
+# print(v.strip())
+# print(v.replace("Hello","hey"))
+# print(v.split("W"))
+
+
+# a= "Hello"
+# b="World"
+# c= a+" "+b
+# print(c)
+
+# age=36
+# txt = "My name is John, and I am {}"
+# print(txt.format(age))
+
+# quantity = 3
+# itemno = 565
+# price = 49.95
+# myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+# print(myorder.format(quantity, itemno, price))
+
+
+# txt = "we are the so-CALLED \"Vikings\" from the north."
+# txt1 = "We are the so-called \fVikings from the north."
+# print(txt1)
+# print(txt.title())
+
+
+# print(10>9)
+# print(10 == 9)
+# print(10<9)
+
+# a = 200
+# b = 33
+
+# if b > a:
+#     print("b is greater then a")
+# else:
+#     print("b is not greater than a")    
+
+
+# print(bool("Hello"))
+# print(bool(15))
+
+# x = "Hello"
+# y = 15
+
+# print(bool(x))
+# print(bool(y))
+
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+# print(bool(()))
+    
+# class myclass():
+#     def __len__(self):
+#         return 0
+
+# myobj = myclass()
+# print(bool(myobj))
+
+# def myFunction():
+#     return False
+
+# print(myFunction())
+
+# from operator import truediv
+
+
+# def myFunction():
+#     return True
+
+# if myFunction():
+#     print("YES!")
+# else:
+#     print("NO!")
+
+# x = 200
+# print(isinstance(x,int))
+
+# thislist = ["apple", "banana", "cherry"]
+# print(type(thislist))
+
+# import this
+
+
+# import this
+
+
+# thislist = list(("apple", "banana", "cherry", "orange", "kiwi", "melon", "mongo"))
+# print(thislist[-4:])
+# if "apple" in thislist:
+#     print("yes, 'apple' is in the fruits list")
+# else:
+#     print("no, 'this' item is not present in thislist")
+
+
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# thislist[1:4] = ["nitin", "kumar", "sutrakar"]
+# print(thislist)
+
+# x = ["apple", "banana", "cherry"]
+# y = ["mango", "pineapple", "papaya"]
+# y = ("kiwi", "orange")
+# x.extend(y)
+
+# x[1:2] = ["nitin", "kumar"]
+# x.append("watermelon")
+# x.pop(1)
+# del x[0]
+# x.clear()
+# print(x)
+
+# from hashlib import new
+
+
+# months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+# newList = []
+
+# for x in months:
+#     print(x)
+
+# for i in range(len(months)):
+#     print(months[i])
+
+# i = 0
+# while i < len(months):
+#     print(months[i])
+#     i = i + 1
+
+# [print(x) for x in months]
+
+# for x in months:
+#     if "J" in x:
+#         newList.append(x)
+
+# print(newList)
+
+# months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+# newlist = [x for x in months if "J" in x]
+# newlist = [x for x in months if x != ("March")]
+# newlist = [x for x in months]
+# newlist = [x for x in range(10)]
+# newlist = [x for x in range(10) if x>5]
+# newlist = [x.upper() for x in months]
+# newlist = ['hello' for x in months]
+
+
+# print(newlist)
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+# newlist = [x if x != "banana" else "orange" for x in fruits]
+
+# newlist = [x for x in fruits]
+# newlist = [x for x in range(10) if x<5]
+# newlist = [x.upper() for x in fruits]
+# newlist = ["Hello" for x in fruits]
+# newlist = [x if x != "banana" else "Orange" for x in fruits]
+
+# print(newlist)
+
+# from operator import truediv
+
+
+# fruits = ["orange", "mango", "kiwi", "pineapple", "banana"]
+
+# fruits.sort(reverse=True)
+
+# print(fruits)
+
+# import this
+
+
+# def myfunc(n):
+#     return abs(n - 50)
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(key=myfunc)
+# print(thislist)
+
+# def myfunc(n):
+#   return abs(n - 50)
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(key = myfunc)
+# print(thislist)
+
+
+# from ast import keyword
+
+
+# thislist = ["banana", "Orange", "Kiwi", "cherry"]
+# thislist.sort(key=str.lower)
+
+# thislist.reverse()
+
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+
+# mylist = list(thislist)
+
+# print(mylist)
+
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+
+# list1.extend(list2)
+
+# print(list1)
+
+
+# thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+
+# thistuple = ("apple",)
+
+# print(type(thistuple))
+
+
+# print(len(thistuple))
+
+
+# mytuple = ("apple", "banana", "cherry")
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+
+# print(type(mytuple))
+# print(thistuple[-4:-0])
+# if "apple" in thistuple:
+#     print("yes, 'apple' is in the tuple")
+
+# y = list(thistuple)
+# y[0] = "kiwi"
+# thistuple = tuple(y)
+
+# y = list(thistuple)
+# y.append("Orange")
+# thistuple = tuple(y)
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+
+# y = ("Apple",)
+# thistuple += y
+
+# print(thistuple)
+
+# y = list(thistuple)
+# y.remove("apple")
+
+# thistuple = tuple(y)
+# print(thistuple)
+
+# del thistuple
+
+# print(thistuple)
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+
+# y = ("Orange",)
+# thistuple += y
+# print(thistuple)
+
+# y = list(thistuple)
+# y.remove("apple")
+# thistuple = tuple(y)
+# print(type(thistuple))
+# print(thistuple)
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+
+# import re
+
+
+# fruits = ("apple", "banana", "cherry")
+
+# (green, yellow, red) = fruits
+
+# print(green, yellow, red)
+
+# fruits = ("apple", "banana", "cherry", "strawberry", "respberry")
+
+# (green, *yellow, red) = fruits
+# print(green,yellow, red)
+
+# for x in fruits:
+#     print(x)
+
+# for i in range(len(fruits)):
+#     print(fruits[i])
+
+# i = 0
+# while i < len(fruits):
+#     print(fruits[i])
+#     i = i + 1
+
+# tuple1 = ("a", "b", "c")
+# tuple2 = (1, 2, 3)
+
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+# fruits = ("apple", "banana", "cherry")
+# mytuple = fruits * 2
+
+# print(mytuple.index("apple"))
+
+# thisset = {"banana", "cherry", "apple"}
+# print(type(thisset))
+
+# import this
+
+
+# thisset = set(("apple", "banana", "cherry"))
+# for x in thisset:
+#     print(x)
+
+# print("banana" in thisset)
+
+
+# print(thisset)
+
+# tropical = ["pineapple", "mango", "papaya"]
+
+# thisset.update(tropical)
+
+# x = thisset.pop()
+
+# print(x)
+# del tropical
+
+# print(tropical)
+
+# set1 = {"a", "b", "c"}
+# set2 = {1,2,3}
+
+# set3 = set1.union(set2)
+
+# print(set3)
+
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+
+# x.symmetric_difference_update(y)
+
+# print(x)
+
+# thisdict = {
+#     "brand": "Ford",
+#     "model": "Mustang",
+#     "year": 1964,
+#     "year": 1453,
+#     "year": 145334
+# }
+
+# import this
+
+
+# from pyexpat import model
+
+
+thisdict = {
+    "brand": "Ford",
+    "electric": False,
+    "year": 1954,
+    "colors": ["red", "white", "blue"]
+}
+
+x = thisdict.keys()
+
+print(x)
