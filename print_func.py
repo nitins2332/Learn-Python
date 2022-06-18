34593b3f61ee832687d351c771892dd02b8cca69
Nitins <nitins2332@gmail.com>
2022-06-18 10:14:47 +0530

    learn python

--- print_func.py
+++ print_func.py
@@ -842,4 +842,93 @@ this is a third line of comment
 # del p1
 # p1.func()
 
+# class Student:
+#    pass
+
+# harry = Student()
+# larry = Student()
+
+# harry.name = "Harry"
+# harry.std =  12
+# harry.section = 1
+
+# larry.std = 9
+# larry.subject = ["hindi", "physics"]
+
+# print(harry.section, larry.subject)
+
+
+# class Employee:
+#     no_of_leaves = 8
+#     pass
+
+# harry = Employee()
+# rohan = Employee()
+
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+
+# rohan.name = "Rohan"
+# rohan.salary = 4554
+# rohan.role = "Student"
+
+# print(Employee.no_of_leaves)
+# print(rohan.__dict__)
+
+# Employee.no_of_leaves = 9
+# print(rohan.__dict__)
+# print(Employee.__dict__)
+
+# print(Employee.no_of_leaves)
+# print(rohan.no_of_leaves)
+
+# print(Employee.no_of_leaves)
+# print(Employee.__dict__)
+# rohan.no_of_leaves = 9
+# print(rohan.__dict__)
+# print(Employee.no_of_leaves)
+
+
+# class Employee:
+#     no_of_leaves = 8
+    
+#     def __init__(self, aname, asalary, arole):
+#         self.name = aname
+#         self.salary = asalary
+#         self.role = arole
+
+#     def printdetails(self):
+#         return f"Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+
+#     classmethod
+#     def change_leaves(cls, newleaves):
+#         cls.no_of_leaves = newleaves
+
+    # classmethod
+    # def from_str(cls, string):
+        # params = string.split("-")
+        # print(params)
+        # return cls(params[0], params[1], params[2])
+        # return cls(*string.split("-"))
+
+# harry = Employee("Harry", 3445, "instrustor")
+# rohan = Employee.from_str("rohat-3444-er")
+
+# harry.change_leaves(34)
+# print(rohan)
+
+# print(rohan.role)
+
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+
+# rohan.name = "Rohan"
+# rohan.salary = 4554
+# rohan.role = "Student"
+
+# print(harry.printdetails())
+
+
 
